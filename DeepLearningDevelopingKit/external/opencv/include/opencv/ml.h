71af37be174200765561530395d409f71cf0e991
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2020-09-22 13:48:04 +0000

    tcp: Do not apply TSO segment limit to non-TSO packets

---
+++ DeepLearningDevelopingKit/external/opencv/include/opencv/ml.h
@@ -0,0 +1,15 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                        Intel License Agreement
+//
+// Copyright (C) 2000, Intel Corporation, all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redis
