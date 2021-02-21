fe7bc09c2ba65c22aa1d05de03f170e2e5bec9c3
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2021-02-21 12:00:42 +0000

    devel commit

--- DeepLearningDevelopingKit/external/opencv/include/opencv2/core/cuda_stream_accessor.hpp
+++ DeepLearningDevelopingKit/external/opencv/include/opencv2/core/cuda_stream_accessor.hpp
@@ -51,4 +51,36 @@
  * This is only header file that depends on CUDA Runtime API. All other headers are independent.
  */
 
-#include <cuda_run
+#include <cuda_runtime.h>
+#include "opencv2/core/cuda.hpp"
+
+namespace cv
+{
+    namespace cuda
+    {
+
+//! @addtogroup cudacore_struct
+//! @{
+
+        /** @brief Class that enables getting cudaStream_t from cuda::Stream
+         */
+        struct StreamAccessor
+        {
+            CV_EXPORTS static cudaStream_t getStream(const Stream& stream);
+            CV_EXPORTS static Stream wrapStream(cudaStream_t stream);
+        };
+
+        /** @brief Class that enables getting cudaEvent_t from cuda::Event
+         */
+        struct EventAccessor
+        {
+            CV_EXPORTS static cudaEvent_t getEvent(const Event& event);
+            CV_EXPORTS static Event wrapEvent(cudaEvent_t event);
+        };
+
+//! @}
+
+    }
+}
+
+#endif /* OPENCV_CORE_CUDA_STREAM_ACCESSOR_HPP */
