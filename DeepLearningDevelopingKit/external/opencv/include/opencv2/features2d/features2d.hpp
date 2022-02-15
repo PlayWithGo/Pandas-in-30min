09a4e80d63171412420d5e91d499215f4195179c
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2022-02-15 21:33:35 +0000

    Updated 404

--- DeepLearningDevelopingKit/external/opencv/include/opencv2/features2d/features2d.hpp
+++ DeepLearningDevelopingKit/external/opencv/include/opencv2/features2d/features2d.hpp
@@ -7,9 +7,11 @@
 //  copy or use the software.
 //
 //
-//                           License Agreement
+//                          License Agreement
 //                For Open Source Computer Vision Library
 //
+// Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
+// Copyright (C) 2009, Willow Garage Inc., all rights reserved.
 // Copyright (C) 2013, OpenCV Foundation, all rights reserved.
 // Third party copyrights are property of their respective owners.
 //
@@ -34,31 +36,4 @@
 // (including, but not limited to, procurement of substitute goods or services;
 // loss of use, data, or profits; or business interruption) however caused
 // and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
-
-#ifndef OPENCV_DNN_HPP
-#define OPENCV_DNN_HPP
-
-// This is an umbrealla header to include into you project.
-// We are free to change headers layout in dnn subfolder, so please include
-// this header for future compatibility
-
-
-/** @defgroup dnn Deep Neural Network module
-  @{
-    This module contains:
-        - API for new layers creation, layers are building bricks of neural networks;
-        - set of built-in most-useful Layers;
-        - API to constuct and modify comprehensive neural networks from layers;
-        - functionality for loading serialized networks models from different frameworks.
-
-    Functionality of this module is designed only for forward pass computations (i. e. network testing).
-    A network training is in principle not supported.
-  @}
-*/
-#include <opencv2/dnn/dnn.hpp>
-
-#endif /* OPENCV_DNN_HPP */
+// or tort (including negligence or otherwise) aris
