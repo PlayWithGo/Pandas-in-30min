3cfccda1878979230186692cb86a51cd86d55ef1
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-01-09 06:57:15 +0000

    Update test

---
+++ DeepLearningDevelopingKit/src/Algorithm/MatrixAnalysis/LUD.hpp
@@ -0,0 +1,51 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	       LU Decomposotion 	                                                    */
+/*								        		 	                Matrix   	                                                              */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+#include "..\..\MathLib\MathLib.h"
+
+
+namespace MathLib
+{
+	namespace MatrixDecomposition
+	{
+		// LU decomposition
+		/// Factors a matrix as the product of a lower triangular matrix and an upper triangular matrix.
+		enum class LUDMethod {
+			Doolittle,
+			Crout,
+			Cholesky
+		};
+
+		template<class T>
+		std::pair<MathLib::Matrix<T>, MathLib::Matrix<T>> LUD(
+			const MathLib::Matrix<T> & _mat, const LUDMethod & _method);
+
+		// Doolittle Method
+		template<class T>
+		std::pair<MathLib::Matrix<T>, MathLib::Matrix<T>> Doolittle(const MathLib::Matrix<T> & _mat);
+
+		// Crout Method
+		template<class T>
+		std::pair<MathLib::Matrix<T>, MathLib::Matrix<T>> Crout(const MathLib::Matrix<T> & _mat);
+
+		template<class T>
+		T LU_Determinant(const MathLib::Matrix<T> & _mat);
+	}
+}
+
+
+namespace MathLib
+{
+	namespace MatrixDecomposition
+	{
+		template<class T>
+		std::pair<MathLib::Matrix<T>, MathLib::Matrix<T>> LUD(const MathLib::Matrix<T>& _mat, const LUDMethod & _method)
+		{
+			switch (_method)
+			{
+			case MathLib::MatrixDecomposit
