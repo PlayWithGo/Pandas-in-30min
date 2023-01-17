d93dbd21d8332b7e710da1aff0450d9ec37fb17a
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-01-17 14:28:51 +0000

    v1.2.12

--- DeepLearningDevelopingKit/src/Algorithm/NeuralNetwork/ConvolutionalNeuralNetwork/CNN_ConvolutionalLayer.h
+++ DeepLearningDevelopingKit/src/Algorithm/NeuralNetwork/ConvolutionalNeuralNetwork/CNN_ConvolutionalLayer.h
@@ -1,19 +1,21 @@
 ﻿/***************************************************************************************************/
 /*                                               Deep Learning Developing Kit                                                   */
-/*								        		   Convolutinal Neural Network     	                                          */
+/*								        		 Convolutional Neural Network     	                                          */
+/*								        		 	    Convolutional Layer     	                                                   */
 /*                                                   www.tianshicangxie.com                                                        */
 /*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
 /***************************************************************************************************/
 #pragma once
 
 // Header files
-#include "CNN_ConvolutionalLayer.h"
-#include "CNN_PoolingLayer.h"
+#include "..\..\..\MathLib\MathLib.h"
+#include "..\ActivationFunction.h"
+#include "..\LossFunction.h"
+#include "CNN_PaddingLayer.h"
 
 /***************************************************************************************************/
-// Namespace : Neural
+// Namespace : Neural 
 /// Provide Neural Network algorithm library.
 namespace Neural
 {
-
-}
+	// Define the E
