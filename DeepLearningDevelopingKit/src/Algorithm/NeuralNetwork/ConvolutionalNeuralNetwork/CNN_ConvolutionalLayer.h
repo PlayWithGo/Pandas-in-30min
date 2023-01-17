b5618bcfe0ddef182ce16775cf072f5c60bf4640
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-01-17 14:35:03 +0000

    Credits

--- DeepLearningDevelopingKit/src/Algorithm/NeuralNetwork/ConvolutionalNeuralNetwork/CNN_ConvolutionalLayer.h
+++ DeepLearningDevelopingKit/src/Algorithm/NeuralNetwork/ConvolutionalNeuralNetwork/CNN_ConvolutionalLayer.h
@@ -18,4 +18,71 @@
 /// Provide Neural Network algorithm library.
 namespace Neural
 {
-	// Define the E
+	// Define the Element datatype.
+	/// Mainly using float and double.
+	typedef double ElemType;
+
+	// Convolutional Layer Initor
+	/// Used for initialization of a ConvLayer.
+	struct ConvLayerInitor
+	{
+		// Stride
+		size_t Stride;
+		// The numner of kernels.
+		size_t KernelNum;
+		// Size of input matrix. 
+		MathLib::Size InputSize;
+		// Size of convolutional kernel.
+		MathLib::Size KernelSize;
+		// Method of padding.
+		PaddingMethod PaddingMethod;
+		// Value uesd in Padding
+		PaddingNum PaddingNum;
+		// Activation Function
+		ActivationFunction ActivationFunction;
+	};
+
+	// Define Kernel and Feature.
+	typedef MathLib::Matrix<ElemType> ConvKernel;
+	typedef MathLib::Matrix<ElemType> ConvFeature;
+
+	/***************************************************************************************************/
+	// Struct : ConvNode
+	/// Node in convolutional layer.
+	struct ConvNode
+	{
+		ConvNode() = default;
+
+		ConvNode(const size_t _kernelM, const size_t _kernelN, const size_t _featureM, const size_t _featureN) {
+			kernel.Init(_kernelM, _kernelN, MathLib::MatrixType::Random);
+			kernelDelta.Init(_kernelM, _kernelN, MathLib::MatrixType::Zero);
+			kernelDeltaSum.Init(_kernelM, _kernelN, MathLib::MatrixType::Zero);
+			feature.Init(_featureM, _featureN, MathLib::MatrixType::Zero);
+			bias = MathLib::Random();
+			biasDelta = 0;
+			biasDeltaSum = 0;
+		}
+
+		ConvNode(const MathLib::Size _kernelSize, const MathLib::Size _featureSize) {
+			kernel.Init(_kernelSize.m, _kernelSize.n, MathLib::MatrixType::Random);
+			kernelDelta.Init(_kernelSize.m, _kernelSize.n, MathLib::MatrixType::Zero);
+			kernelDeltaSum.Init(_kernelSize.m, _kernelSize.n, MathLib::MatrixType::Zero);
+			feature.Init(_featureSize.m, _featureSize.n, MathLib::MatrixType::Zero);
+			bias = MathLib::Random();
+			biasDelta = 0;
+			biasDeltaSum = 0;
+		}
+
+		ConvKernel kernel;
+		ElemType bias;
+		ConvFeature feature;
+
+		ConvKernel kernelDelta;
+		ElemType biasDelta;
+
+		ConvKernel kernelDeltaSum;
+		ElemType biasDeltaSum;
+	};
+
+	/***************************************************************************************************/
+	// Class : 
