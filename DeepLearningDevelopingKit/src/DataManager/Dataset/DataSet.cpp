557506898edf838f0d40b047030da5488bc92b10
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-02-03 18:36:33 +0000

    fix for travis

---
+++ DeepLearningDevelopingKit/src/DataManager/Dataset/DataSet.cpp
@@ -0,0 +1,6 @@
+
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	              DataSet     	                                                               */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
