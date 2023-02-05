826accde67c4d9a034004aa1f31e30ef46324ad1
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-02-05 12:37:58 +0000

    renamed.

---
+++ DeepLearningDevelopingKit/src/DataManager/SaveLoad/Saver.cpp
@@ -0,0 +1,12 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 			    Data Saver                                                                  */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+#include "Saver.h"
+
+void Save::SaveNode(const std::string & _filePath, const InputNode & _node)
+{
+	// VectorParser::Serialize(_filePath, );
+}
---
+++ DeepLearningDevelopingKit/src/DataManager/SaveLoad/Saver.h
@@ -0,0 +1,19 @@
+
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 			    Data Saver                                                                  */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "..\..\Util\Json\JsonParser.h"
+
+class Save
+{
+public:
+	static void SaveNode(const std::string & _filePath, const InputNode & _node);
+};
