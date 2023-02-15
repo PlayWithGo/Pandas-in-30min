bc3c7053375318d66d4ca8c70ee37e836be7ec63
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-02-15 09:14:02 +0000

    Added to .gitignore

--- DeepLearningDevelopingKit/src/UnitTest/CNN_SerializeLayer_Test.cpp
+++ DeepLearningDevelopingKit/src/UnitTest/CNN_SerializeLayer_Test.cpp
@@ -16,4 +16,23 @@ int main(int argc, char ** argv)
 	const size_t deserializedSizeN = 1;
 	const size_t deserializedNum = 5;
 	Neural::SerializeLayerInitor serialInitor;
-	serialInitor.Seriali
+	serialInitor.SerializeSize = MathLib::Size(deserializedSizeM*deserializedSizeN*deserializedNum, 1);
+	serialInitor.DeserializeSize = MathLib::Size(deserializedSizeM, deserializedSizeN);
+	Neural::SerializeLayer serial(serialInitor);
+
+	MathLib::Matrix<double> serializedMat(deserializedSizeM*deserializedSizeN*deserializedNum, 1, MathLib::MatrixType::Random);
+
+	serial.SetSerializedMat(serializedMat);
+	std::vector<MathLib::Matrix<double>> deserializedMat = serial.Deserialize();
+	serializedMat = serial.Serialize();
+
+	for(auto mat: deserializedMat)
+		std::cout<< mat<<std::endl;
+
+	std::cout << serializedMat<<std::endl;
+
+	system("pause");
+	return 0;
+}
+
+#endif
---
+++ DeepLearningDevelopingKit/src/UnitTest/JsonHandler_test.cpp
@@ -0,0 +1,19 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 		 Json Handler Test	                                                              */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define JsonHandlerDebug
+
+#ifdef JsonHandlerDebug
+#include <iostream>
+#include <string>
+#include "..\MathLib\MathLib.h"
+#include "..\Util\Json\JsonHandler.h"
+#include "..\Util\Json\JsonParser.h"
+#include "..\Util\Timer\Timer.h"
+
+using namespace std;
+using n
