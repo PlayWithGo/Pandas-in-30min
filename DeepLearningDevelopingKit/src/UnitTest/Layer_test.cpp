13cab824a4bdbfc92422afcae15e0caf9cd9b8cf
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-02-19 09:56:33 +0000

    Update core.c

---
+++ DeepLearningDevelopingKit/src/UnitTest/Layer_test.cpp
@@ -0,0 +1,23 @@
+﻿/***************************************************************************************************/
+/*                                               Deep Learning Developing Kit                                                   */
+/*								        		 	           Layer Test   	                                                              */
+/*                                                   www.tianshicangxie.com                                                        */
+/*                                      Copyright © 2015-2018 Celestial Tech Inc.                                          */
+/***************************************************************************************************/
+
+// #define LayerDebug
+
+#ifdef LayerDebug
+
+// Header files
+#include <iostream>
+#include "..\MathLib\MathLib.h"
+#include "..\Neural\NeuralLib.h"
+#include "..\Util\Log.h"
+#include <ctime>
+#include "..\MathLib\ActivationFunction.h"
+
+// Namespaces
+using namespace std;
+using namespace MathLib;
+using namespac
