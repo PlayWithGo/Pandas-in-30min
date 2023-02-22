cb29f277c3cc31c8949bef56b39a8d9f66675df6
PlayWithGo <118044531+PlayWithGo@users.noreply.github.com>
2023-02-22 09:33:21 +0000

    add bower config

--- DeepLearningDevelopingKit/src/Visualizer/Plot/Plot.hpp
+++ DeepLearningDevelopingKit/src/Visualizer/Plot/Plot.hpp
@@ -38,4 +38,70 @@ namespace Visual
 		static cv::Mat Scale(const cv::Mat _mat, unsigned n);
 
 		template<class T>
-		static void PlotCNNTrainProcess(const std::vector<std::vector<MathLib::Matrix<T>>> _data, const std::string
+		static void PlotCNNTrainProcess(const std::vector<std::vector<MathLib::Matrix<T>>> _data, const std::string & _name = "Layer1");
+	};
+
+	template<class T>
+	inline void Plot2D::Plot2DMatrix(const MathLib::Matrix<T>& _mat, const std::string & _name, const Plot2DMode _mode, unsigned _x, unsigned _y)
+	{
+		cv::Mat tempMat(cv::Size(_mat.ColumeSize(), _mat.RowSize()), CV_32FC1);
+		tempMat = Visual::OpenCV::Matrix2Mat<float>(_mat);
+		tempMat += 1;
+		cv::normalize(tempMat, tempMat, 0, 1, cv::NORM_MINMAX);
+
+		cv::Mat img(tempMat.cols, tempMat.rows, CV_8UC3);
+		for (size_t i = 0; i < tempMat.cols; i++)
+		{
+			for (size_t j = 0; j < tempMat.rows; j++)
+			{
+				cv::Vec3b pixel;
+				switch (_mode)
+				{
+				case Plot2DMode::R:
+					pixel.val[0] = 0; // B
+					pixel.val[1] = 0; // G
+					pixel.val[2] = tempMat.at<float>(i, j) * 255; // R
+					break;
+				case Plot2DMode::G:
+					pixel.val[0] = 0; // B
+					pixel.val[1] = tempMat.at<float>(i, j) * 255; // G
+					pixel.val[2] = 0; // R
+					break;
+				case Plot2DMode::B:
+					pixel.val[0] = tempMat.at<float>(i, j) * 255; // B
+					pixel.val[1] = 0; // G
+					pixel.val[2] = 0; // R
+					break;
+				case Plot2DMode::RG:
+					pixel.val[0] = 0; // B
+					pixel.val[1] = (1 - tempMat.at<float>(i, j)) * 255; // G
+					pixel.val[2] = tempMat.at<float>(i, j) * 255; // R
+					break;
+				case Plot2DMode::GB:
+					pixel.val[0] = 0; // B
+					pixel.val[1] = (1 - tempMat.at<float>(i, j)) * 255; // G
+					pixel.val[2] = tempMat.at<float>(i, j) * 255; // R
+					break;
+				case Plot2DMode::RB:
+					pixel.val[0] = (1 - tempMat.at<float>(i, j)) * 255; // B
+					pixel.val[1] = 0; // G
+					pixel.val[2] = tempMat.at<float>(i, j) * 255; // R
+					break;
+				default:
+					pixel.val[0] = (1 - tempMat.at<float>(i, j)) * 255; // B
+					pixel.val[1] = 0; // G
+					pixel.val[2] = tempMat.at<float>(i, j) * 255; // R
+					break;
+				}
+				img.at<cv::Vec3b>(i, j) = pixel;
+			}
+		}
+		resize(img, img, cv::Size(img.cols * 10, img.rows * 10), 0, 0, cv::INTER_NEAREST);
+
+		cv::namedWindow(_name, cv::WINDOW_AUTOSIZE);
+		cv::imshow(_name, img);
+		cv::waitKey(10);
+	}
+
+	template<class T>
+	inline static void Plot2D::Plot2DMatrixVec(const std::vector<MathLib::M
